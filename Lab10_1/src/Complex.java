8031e36b81363c70a3d26f30587db700f7f478d9
Protsenko Anna <aaprtsnk@gmail.com>
2020-11-29 16:10:54 +0300

    Практическая работа №10

---
+++ Lab10_1/src/Complex.java
@@ -0,0 +1,27 @@
+public class Complex {
+    private int real, image;
+
+    public Complex() {
+    }
+
+    public Complex(int real, int image) {
+        this.real = real;
+        this.image = image;
+    }
+
+    public int getReal() {
+        return real;
+    }
+
+    public void setReal(int real) {
+        this.real = real;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
---
+++ Lab10_2/src/VictorianChair.java
@@ -0,0 +1,18 @@
+public class VictorianChair implements Chair {
+    private int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public VictorianChair() {
+    }
+
+    public VictorianChair(int age) {
+        this.age = age;
+    }
+}
